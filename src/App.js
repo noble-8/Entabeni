9e15cca7494eb8644de152a07e3818d23ebe3cb6
Nair <akash7nair@gmail.com>
2023-02-04 10:44:47 -0500

    fix typo, add edit delete button

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React, { useState } from 'react';
 
-const SkiResort = ({ resort }) => (
+const SkiResort = ({ resort, onDelete, onUpdate }) => (
   <li>
     {resort.name}, {resort.location}, {resort.number_of_ski_runs}
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    <button onClick={onDelete}>Delete</button>
+    &nbsp;
+    &nbsp;
+    &nbsp;
+    <button onClick={() => onUpdate(resort)}>Edit</button>
   </li>
 );
 
 const SkiResorts = () => {
+  
   const [resorts, setResorts] = useState([
     { name: 'Squaw Valley', location: 'Lake Tahoe, CA', 'number_of_ski_runs':2 },
     { name: 'Aspen Mountain', location: 'Aspen, CO' , 'number_of_ski_runs':4},
     { name: 'Deer Valley', location: 'Park City, UT' , 'number_of_ski_runs':5},
   ]);
+
 
   const [newResort, setNewResort] = useState({ name: '', location: '', number_of_ski_runs: 0});
 
@@ -70,7 +80,7 @@ const SkiResorts = () => {
           type="number"
           value={newResort.number_of_ski_runs}
           onChange={(event) =>
-            setNewResort({ ...newResort, location: event.target.value })
+            setNewResort({ ...newResort, number_of_ski_runs: event.target.value })
           }
           placeholder="Number Of ski runs"
         />
